355918672596585c3809d0bc2647a2509d611ad7
Herman <hermanh@student.chalmers.se>
2016-05-30 17:51:46 +0200

    Fixing creation 1

---
+++ divide_and_conquer.h
@@ -0,0 +1,30 @@
+#ifndef DIVIDE_AND_CONQUER_H
+#define DIVIDE_AND_CONQUER_H
+
+#include <cstddef>
+#include "datastructures/tmp/tmp_types.h"
+
+namespace alg
+{
+    namespace detail
+    {
+    } // namespace detail
+
+    template <typename T, std::size_t N>
+    tmp::tuple_t<T...> divide_and_conquer(const T&& ... ts)
+    {
+
+    }
+
+    template <typename ITERATOR, std::size_t N>
+    void divide_and_conquer(ITERATOR & begin, const ITERATOR & end)
+    {
+
+    }
+
+
+} // namespace alg
+
+
+
+#endif /* DIVIDE_AND_CONQUER_H */
